7197c40fd979cc504a8722f1b22f6707049fd9f0
Raghu <raghurama.j@gmail.com>
2016-11-09 10:03:42 +0530

    new branch

--- portal/src/main/java/com/java/portal/controllers/HomeController.java
+++ portal/src/main/java/com/java/portal/controllers/HomeController.java
@@ -49,6 +49,7 @@ public class HomeController {
 	
 	@RequestMapping(value = {"/", "home"}, method = RequestMethod.GET)
 	public String home() {
+		log.info("venesa");
 		return "home";
 	}
 	@RequestMapping(value = {"/facebook"}, method = RequestMethod.GET)
